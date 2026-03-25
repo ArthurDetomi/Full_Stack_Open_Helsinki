60ec91dd85c56e2946d25864675e74e9809c74b6
Arthur Detomi <arthurdetomi@gmail.com>
2026-03-25 08:57:51 -0300

    PART 9: exercise 9.4 completed

---
+++ part9/first_steps/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+const PORT = 3003;
+
+app.get("/hello", (_req: Request, res: Response) => {
+  res.send("Hello Full Stack");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
