cf6a3ca6ef13abe6c5287f4abefe6a045fbbf8bc
Arthur Detomi <arthurdetomi@gmail.com>
2026-04-13 08:47:23 -0300

    Part 9: exercise 9.20 completed

--- part9/ilaris/front/ilaris-front/src/components/DiaryForm.tsx
+++ part9/ilaris/front/ilaris-front/src/components/DiaryForm.tsx
@@ -34,18 +34,36 @@ const DiaryForm = (props: DiaryFormProps) => {
     }
   };
 
+  const handleChangeVisibility = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setDiary({
+      ...diary,
+      visibility: event.target.value as Visibility,
+    });
+  };
+
+  const handleChangeWeather = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDiary({
+      ...diary,
+      weather: event.target.value as Weather,
+    });
+  };
+
   return (
     <>
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       <form onSubmit={handleSubmitDiary}>
         <div>
-          <label htmlFor="date">date</label>
+          <label htmlFor="date">Date:</label>
           <input
-            type="text"
+            type="date"
             id="date"
-            value={diary.date}
             name="date"
+            value={diary.date}
+            min="2018-01-01"
+            max="2018-12-31"
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               setDiary({
                 ...diary,
@@ -55,34 +73,123 @@ const DiaryForm = (props: DiaryFormProps) => {
           />
         </div>
         <div>
-          <label htmlFor="visibility">visibility</label>
-          <input
-            type="text"
-            id="visibility"
-            value={diary.visibility}
-            name="visibility"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setDiary({
-                ...diary,
-                visibility: event.target.value as Visibility,
-              });
-            }}
-          />
+          <fieldset>
+            <legend>visibility:</legend>
+
+            <div>
+              <input
+                type="radio"
+                id="great"
+                name="visibility"
+                value="great"
+                checked={diary.visibility === "great"}
+                onChange={handleChangeVisibility}
+              />
+              <label htmlFor="great">great</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="good"
+                name="visibility"
+                value="good"
+                checked={diary.visibility === "good"}
+                onChange={handleChangeVisibility}
+              />
+              <label htmlFor="good">good</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="ok"
+                name="visibility"
+                value="ok"
+                checked={diary.visibility === "ok"}
+                onChange={handleChangeVisibility}
+              />
+              <label htmlFor="ok">ok</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="poor"
+                name="visibility"
+                value="poor"
+                checked={diary.visibility === "poor"}
+                onChange={handleChangeVisibility}
+              />
+              <label htmlFor="poor">poor</label>
+            </div>
+          </fieldset>
         </div>
+
         <div>
-          <label htmlFor="weather">weather</label>
-          <input
-            type="text"
-            id="weather"
-            value={diary.weather}
-            name="weather"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setDiary({
-                ...diary,
-                weather: event.target.value as Weather,
-              });
-            }}
-          />
+          <fieldset>
+            <legend>weather:</legend>
+
+            <div>
+              <input
+                type="radio"
+                id="sunny"
+                name="weather"
+                value="sunny"
+                checked={diary.weather === "sunny"}
+                onChange={handleChangeWeather}
+              />
+              <label htmlFor="sunny">sunny</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="rainy"
+                name="weather"
+                value="rainy"
+                checked={diary.weather === "rainy"}
+                onChange={handleChangeWeather}
+              />
+              <label htmlFor="rainy">rainy</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="cloudy"
+                name="weather"
+                value="cloudy"
+                checked={diary.weather === "cloudy"}
+                onChange={handleChangeWeather}
+              />
+              <label htmlFor="cloudy">cloudy</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="stormy"
+                name="weather"
+                value="stormy"
+                checked={diary.weather === "stormy"}
+                onChange={handleChangeWeather}
+              />
+              <label htmlFor="stormy">stormy</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="windy"
+                name="weather"
+                value="windy"
+                checked={diary.weather === "windy"}
+                onChange={handleChangeWeather}
+              />
+              <label htmlFor="windy">windy</label>
+            </div>
+          </fieldset>
         </div>
         <div>
           <label htmlFor="comment">comment</label>
